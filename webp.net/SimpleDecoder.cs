883c8a5c59bf77e85519fbf25370121bb0c7b221
Nathanael Jones <nathanael.jones@gmail.com>
2012-10-18 18:32:33 -0400

    Added first draft of SimpleEncoder and SimpleDecoder functions

---
+++ webp.net/SimpleDecoder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Imazen.WebP.Extern;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace Imazen.WebP {
+
+
+    public class SimpleDecoder {
+
+        public SimpleDecoder() {
+        }
+
+
+
+        public unsafe Bitmap DecodeFromBytes(byte[] data, long length) {
+            fixed (byte* dataptr = data) {
+                return DecodeFromPointer((IntPtr)dataptr, length);
+            }
+        }
+        public  Bitmap DecodeFromPointer(IntPtr data, long length) {
+            int w = 0, h = 0;
+            //Validate header and determine size
+            if (NativeMethods.WebPGetInfo(data, (uint)length, ref w, ref h) == 0) throw new Exception("Invalid WebP header detected");
+
+            bool success = false;
+            Bitmap b = null;
+            BitmapData bd = null;
+            try {
+                //Allocate canvas
+                b = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                //Lock surface for writing
+                bd = b.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                //Decode to surface
+                IntPtr result =  NativeMethods.WebPDecodeBGRAInto(data, (uint)length, bd.Scan0, (uint)( bd.Stride * bd.Height), bd.Stride);
+                if (bd.Scan0 != result) throw new Exception("Failed to decode WebP image with error " + (long)result);
+                success = true;
+            } finally {
+                //Unlock surface
+                if (bd != null && b != null) b.UnlockBits(bd);
+                //Dispose of bitmap if anything went wrong
+                if (!success && b != null) b.Dispose();
+            }
+            return b;
+        }
+
+    }
+}
---
+++ webp.net/SimpleEncoder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Imazen.WebP.Extern;
+using System.IO;
+
+namespace Imazen.WebP {
+    public class SimpleEncoder {
+        public SimpleEncoder() { }
+
+
+        /// <summary>
+        /// Encodes the given RGB(A) bitmap to the given stream. Specify quality = -1 for lossless, otherwise specify a value between 0 and 100.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="quality"></param>
+        public void Encode(Bitmap from, Stream to, float quality) {
+            IntPtr result;
+            long length;
+
+            Encode(from, quality, out result, out length);
+            try {
+                byte[] buffer = new byte[4096];
+                for (int i = 0; i < length; i += buffer.Length) {
+                    int used = (int)Math.Min((int)buffer.Length, length - i);
+                    Marshal.Copy(result, buffer, i, used);
+                    to.Write(buffer, 0, used);
+                }
+            } finally {
+                NativeMethods.WebPFree(result);
+            }
+
+        }
+
+        public void Encode(Bitmap b, float quality, out IntPtr result, out long length) {
+            if (quality < -1) quality = -1;
+            if (quality > 100) quality = 100;
+            int w = b.Width;
+            int h = b.Height;
+            var bd = b.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadOnly, b.PixelFormat);
+            try {
+                result = IntPtr.Zero;
+                if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppArgb){
+                    if (quality == -1) length = NativeMethods.WebPEncodeLosslessBGRA(bd.Scan0, w, h, bd.Stride, ref result);
+                    else length = NativeMethods.WebPEncodeBGRA(bd.Scan0, w, h, bd.Stride, quality, ref result);
+                }else if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppRgb){
+                    if (quality == -1) length = NativeMethods.WebPEncodeLosslessBGRA(bd.Scan0, w, h, bd.Stride, ref result);
+                    else length = NativeMethods.WebPEncodeBGRA(bd.Scan0, w, h, bd.Stride, quality, ref result);
+                }else{
+                    throw new NotSupportedException("Only Format32bppArgb and Format32bppRgb bitmaps are supported");
+                }
+                if (length == 0) throw new Exception("WebP encode failed!");
+
+            } finally {
+                b.UnlockBits(bd);
+            }
+        }
+    }
+}
