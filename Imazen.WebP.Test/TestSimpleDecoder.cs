82cddcb07f323426f1d271688f5d368b00059c4c
meese <matt.w.abboud@gmail.com>
2014-06-04 21:03:43 -0600

    Update to libwebp 0.4.0
    Updated to libwebp 0.4.0 and added basic unit test project

---
+++ Imazen.WebP.Test/TestSimpleDecoder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Imazen.WebP;
+using System.Drawing;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestSimpleDecoder
+    {
+        [TestMethod]
+        public void TestDecSimple()
+        {
+
+            var decoder = new SimpleDecoder();
+            var fileName = "testimage.webp";
+            var outFile = "testimageout.jpg";
+            File.Delete(outFile);
+            FileStream outStream = new FileStream(outFile, FileMode.Create);
+            using (Stream inputStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            {
+
+                var bytes = ReadFully(inputStream);
+                var outBitmap = decoder.DecodeFromBytes(bytes, bytes.LongLength);
+                outBitmap.Save(outStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                outStream.Close();
+            }
+
+            FileInfo finfo = new FileInfo(outFile);
+            Assert.IsTrue(finfo.Length > 5000);
+            
+
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+
+
+}
---
+++ Imazen.WebP.Test/TestSimpleEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Imazen.WebP;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Imazen.WebP.Extern;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestSimpleEncoder
+    {
+        [TestMethod]
+        public void TestEncSimple()
+        {
+            var encoder = new SimpleEncoder();
+            var fileName = "testimage.jpg";
+            var outFileName = "testimageout.webp";
+            File.Delete(outFileName);
+
+            Bitmap mBitmap;
+            FileStream outStream = new FileStream(outFileName, FileMode.Create);
+            using (Stream BitmapStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            {
+                Image img = Image.FromStream(BitmapStream);
+
+                mBitmap = new Bitmap(img);
+
+                encoder.Encode(mBitmap, outStream, 100, false);
+            }
+
+            FileInfo finfo = new FileInfo(outFileName);
+            Assert.IsTrue(finfo.Length > 5000);
+        }
+    }
+}
