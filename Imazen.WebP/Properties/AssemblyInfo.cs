55eae2f4a89900b8a82268d008db8639447a4508
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-14 10:00:30 -0400

    Update assembly & file versions to 0.2.0. Add Appveyor.yml file.

--- Imazen.WebP/Properties/AssemblyInfo.cs
+++ Imazen.WebP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Imazen")]
 [assembly: AssemblyProduct("WebP.NET")]
-[assembly: AssemblyCopyright("Copyright © Imazen 2012")]
+[assembly: AssemblyCopyright("Copyright © Imazen 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion(".1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0.*")]
