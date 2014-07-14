9b382dbd9e39f8a3442d4f20b28fcf4483f0768a
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-14 08:04:07 -0400

    Updated to libwebp 0.4.0 and added basic unit test project Fixes to libwebp .40 update
    Changes unit test project to xunit and net 2.0.
    Reduces unit test image size.
    I release these contributions under the terms of the Imazen Contributor
    Agreement V1.0
    (http://imageresizing.net/licenses/contribute), the terms of which I
    accept and agree.
    - Matt Abboud 6/16/2014
    
    Fixed test project (Had 4.5 refs but was 2.0)
    Replaced native binaries with .dlls built from 0.4.0 branch.

--- Imazen.WebP/Extern/Constants.cs
+++ Imazen.WebP/Extern/Constants.cs
@@ -5,11 +5,11 @@ using System.Text;
 namespace Imazen.WebP.Extern {
     public partial class NativeMethods {
 
-        /// WEBP_DECODER_ABI_VERSION -> 0x0200
-        public const int WEBP_DECODER_ABI_VERSION = 512;
+        /// WEBP_DECODER_ABI_VERSION 0x0203    // MAJOR(8b) + MINOR(8b)
+        public const int WEBP_DECODER_ABI_VERSION = 515;
 
-        /// WEBP_ENCODER_ABI_VERSION -> 0x0200
-        public const int WEBP_ENCODER_ABI_VERSION = 512;
+        /// WEBP_ENCODER_ABI_VERSION 0x0202    // MAJOR(8b) + MINOR(8b)
+        public const int WEBP_ENCODER_ABI_VERSION = 514;
 
         /// <summary>
         /// The maximum length of any dimension of a WebP image is 16383
